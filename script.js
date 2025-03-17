cba7998833d0389c5521520c2e6115eac8d5fbec
Alexandre Marchioli <marchioliprogrammer@gmail.com>
2025-03-17 18:38:21 -0300

    finalização da página serviços e inicio da página reviews

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// Código pro menu humburguinho 
+
 function mudouTamanho() {
     if (window.innerWidth >= 768) {
     itens.style.display = 'flex'
@@ -13,3 +16,38 @@ function clickMenu() {
     itens.style.display = 'block'
    }
 }
+
+// Código para o carrocel de imagens
+
+window.onload = function() {
+    showSlider(); 
+};
+
+const slider = document.querySelectorAll('.img-slider');  
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+    slider.forEach(item => item.classList.remove('on'));
+}
+
+function showSlider() {
+    slider[currentSlide].classList.add('on');
+}
+
+function nextSlider() {
+    hideSlider();
+    currentSlide = (currentSlide + 1) % slider.length;
+    showSlider();
+}
+
+function prevSlider() {
+    hideSlider();
+    currentSlide = (currentSlide - 1 + slider.length) % slider.length;
+    showSlider();
+}
+
+btnNext.addEventListener('click', nextSlider);
+btnPrev.addEventListener('click', prevSlider);
